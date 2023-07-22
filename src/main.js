699b0880851b6fbeefd63f7acec0dec625ff6f24
U K I <marzukiaslb@gmail.com>
2023-07-22 09:03:36 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'bootstrap';
+import './assets/sass/custom.scss';
+import './assets/css/main.css';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
